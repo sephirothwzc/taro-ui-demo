8e1b5f95a786de5f5a2d4f324f8049af229e830e
zhanchao.wu <zhanchao.wu@icloud.com>
2022-03-22 18:43:00 +0800

    feat: taro-ui

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,25 +1,26 @@
-import React, { useCallback } from "react";
-import { View, Text, Button, Image } from "@tarojs/components";
-import { useEnv, useNavigationBar, useModal, useToast } from "taro-hooks";
-import logo from "./hook.png";
+import React, { useCallback } from 'react';
+import { View, Text, Button, Image } from '@tarojs/components';
+import { useEnv, useNavigationBar, useModal, useToast } from 'taro-hooks';
+import logo from './hook.png';
 
-import './index.scss'
+import './index.scss';
 
 const Index = () => {
   const env = useEnv();
-  const [_, { setTitle }] = useNavigationBar({ title: "Taro Hooks" });
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_, { setTitle }] = useNavigationBar({ title: 'Taro Hooks' });
   const [show] = useModal({
-    title: "Taro Hooks!",
+    title: 'Taro Hooks!',
     showCancel: false,
-    confirmColor: "#8c2de9",
-    confirmText: "支持一下",
+    confirmColor: '#8c2de9',
+    confirmText: '支持一下',
     mask: true,
   });
   const [showToast] = useToast({ mask: true });
 
   const handleModal = useCallback(() => {
-    show({ content: "不如给一个star⭐️!" }).then(() => {
-      showToast({ title: "点击了支持!" });
+    show({ content: '不如给一个star⭐️!' }).then(() => {
+      showToast({ title: '点击了支持!' });
     });
   }, [show, showToast]);
 
@@ -35,7 +36,7 @@ const Index = () => {
         <Text className="label">运行环境</Text>
         <Text className="note">{env}</Text>
       </View>
-      <Button className="button" onClick={() => setTitle("Taro Hooks Nice!")}>
+      <Button className="button" onClick={() => setTitle('Taro Hooks Nice!')}>
         设置标题
       </Button>
       <Button className="button" onClick={handleModal}>
