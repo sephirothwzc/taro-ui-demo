c9e2a0fb127c0353116c439eab0fa04d88c9e7a0
zhanchao.wu <zhanchao.wu@icloud.com>
2022-03-23 14:04:41 +0800

    feat(immer): immer

--- src/pages/index/components/home/index.tsx
+++ src/pages/index/components/home/index.tsx
@@ -1,7 +1,22 @@
 import { View } from '@tarojs/components';
+import { useImmer } from 'use-immer';
+import { AtButton } from 'taro-ui';
 import React, { FC } from 'react';
 import styles from './index.module.scss';
 
-const Index: FC = () => <View className={styles.layout}>我是工作台</View>;
+const Index: FC = () => {
+  const [title, setTitle] = useImmer('我是工作台');
+  const handleClick = () => {
+    setTitle(() => new Date().toString());
+  };
+  return (
+    <View className={styles.layout}>
+      {title}
+      <AtButton type="primary" onClick={handleClick}>
+        测试
+      </AtButton>
+    </View>
+  );
+};
 
 export default Index;
